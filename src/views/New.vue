eb54aaf7302dd1cfae741abe2c005b4d785691a4
HendrikHahn1996 <hendrik.hahn1996@gmail.com>
2022-06-25 11:40:38 +0200

    New House Page half done

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Create new List</h1>
+    <NavBar />
+    <div class="Container">
+        <dir class="content">
+            <h1>PlaceHolder: Go back to Page</h1>
+
+        </dir>
+    </div>
 </template>
+
+<script>
+import NavBar from '@/compontents/NavBar.vue'
+
+export default {
+    name: 'New',
+    components : {
+        NavBar
+    }
+    
+}
+</script>
+
+<style scoped>
+    .content {
+        width: 50%; 
+        display: grid;
+        margin-left: 21%;
+        margin-top: 1%;
+        background-color: red;
+    }
+    
+</style>
